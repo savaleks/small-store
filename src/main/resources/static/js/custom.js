529b4918b44e96eb41b755dd9c16280802f5a37f
Alexander <savaleks@gmail.com>
2019-04-02 16:58:41 +0300

    Fetched data from database to DataTable

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,23 +1,34 @@
 
 $(document).ready(function () {
 
-// code for jquery dataTable
-    var products = [
-        ['1', 'qwerty'],
-        ['2', 'sdfsdty'],
-        ['3', 'qwfsdfsy'],
-        ['4', 'qwertdfy'],
-        ['5', 'qfsdfty']
-    ];
+    var jsonUrl = '';
+        if (window.categoryId == ''){
+            jsonUrl = /*[[@{/json/data/all/products}]]*/ '/json/data/all/products';
+        } else {
+            jsonUrl = /*[[@{/json/data/category/{id}/products}]]*/ '/json/data/category/'+window.categoryId+'/products';
+        }
 
-    var $table = $('#productListTable');
-    if ($table.length){
-        // console.log('Inside the table');
-
-        $table.DataTable({
-            lengthMenu: [[10, 20, 50, -1], ['10', '20', '50', 'ALL']],
-            pageLength: 10,
-            data: products
-        });
-    }
+    var table = $('#productListTable').DataTable({
+        lengthMenu: [[10, 20, 50, -1], ['10', '20', '50', 'ALL']],
+        pageLength: 10,
+        ajax: {url: jsonUrl, dataSrc: ''},
+        columns:[
+            {data: "code", mRender: function (data, type, row) {
+                   return '<img src="/images/'+data+'.jpg" class="dataTableImg"/>'
+                }},
+            {data: "name"},
+            {data: "brand"},
+            {data: "unitPrice", mRender: function (data, type, row) {
+                    return '&#8364; ' + data
+                }},
+            {data: "quantity"},
+            {data: "id", mRender: function (data, type, row) {
+                    var str = '';
+                    str += '<a href="/show/'+data+'/product" class="btn btn-primary btn-sm">View</a>';
+                    str += '&#160;';
+                    str += '<a href="/cart/add/'+data+'/product" class="btn btn-success btn-sm">Add to Cart</a>';
+                    return str;
+                }}
+        ]
+    })
 });
